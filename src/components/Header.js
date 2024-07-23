40ee0c4e031feddaae5bc8e632869dd9830b1d84
Badmus Damola Khadijah <Damolabadmus014@gmail.com>
2024-07-23 16:30:36 +0100

    text update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ function Header() {
     return (
         <header className=" flex items-center justify-center  mb-6 ">
             <img className=" " src="logo512.png" alt="React Logo"/>
-            <h1 className="  ">THE REACT QUIZ</h1>
+            <h1 className=" ">THE REACT QUIZ</h1>
         </header>
     )
 }
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,8 +2,8 @@
 function Loader() {
      
     return (
-        <div className=" flex item-center justify-center w-full font-roboto">
-            <div className=" flex items-center gap-x-3 text-green-800">
+        <div className=" flex item-center justify-center w-full mt-52 font-roboto">
+            <div className=" flex items-center gap-x-3 text-blue-200 text-3xl">
             <p className="   animate-pulse mt-4  "> Loading questions</p>
             <p className="   animate-bounce text-5xl  "> ...</p>
             </div>
